a77f33164688040512cb386acf03f1e401b56b07
jjf-covello <covellojorge@gmail.com>
2020-04-14 13:05:45 -0300

    agrego password validator

---
+++ GeSoc/src/main/java/PasswordValidator.java
@@ -0,0 +1,14 @@
+public class PasswordValidator {
+    //singleton class
+    static public void validar(String contrasenia){
+        /*
+        Objetivos de este metodo:
+            *
+            *
+            *
+            *
+            *
+        */
+
+    }
+}

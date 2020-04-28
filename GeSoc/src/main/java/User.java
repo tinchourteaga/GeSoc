31ad4165f79913b73f17a44f219b4875743b104e
tinchourteaga <38136900+tinchourteaga@users.noreply.github.com>
2020-04-28 11:17:51 -0300

    Fix
    Agrego excepciones al validate()

--- GeSoc/src/main/java/User.java
+++ GeSoc/src/main/java/User.java
@@ -1,10 +1,12 @@
+import java.io.IOException;
+
 public class User {
 
     private String role;
     private String name;
     private String password;
 
-    private User(String role, String name, String password) throws PasswordException {
+    private User(String role, String name, String password) throws PasswordException, SpecialCharacterException, CommonPasswordException, NumberException, LengthException, IOException {
         this.role = role;
         this.name = name;
         PasswordValidator.validate(password);

eee7bb3fe6f17e8594d5b4968e14b5d87da312b0
jjf-covello <covellojorge@gmail.com>
2020-06-20 11:35:56 -0300

    agrego constructor del Ingreso
    Son dos constructores. Uno para cuando lo tengo asociado a un egreso y otro para cuando no.

--- GeSoc/src/main/java/Ingreso/Ingreso.java
+++ GeSoc/src/main/java/Ingreso/Ingreso.java
@@ -11,11 +11,23 @@ public class Ingreso {
     double valor;
     Date fecha;
     String descripcion;
-    Egreso operacionAsociada;
+    Egreso operacionAsociada= null;
     //De aca para abajo no sabemos si esta bien
     //al final no estaba bien y lo deberiamos dejar sin esas cosas
     //por eso las comento
    /* MetodoPago medioPago;
     DocumentoComercial documentoComercial;
     TipoDeIngreso tipoDeIngreso;*/
+   public Ingreso(double valor,Date fecha,String descripcion){
+       this.valor=valor;
+       this.fecha=fecha;
+       this.descripcion=descripcion;
+   }
+   public Ingreso(double valor,Date fecha,String descripcion,Egreso unaCompra){
+       this.valor=valor;
+       this.fecha=fecha;
+       this.descripcion=descripcion;
+       this.operacionAsociada=unaCompra;
+       //no se si hacerlo con un optional y al joraca pero despues voy a complicarme la vida con eso
+   }
 }

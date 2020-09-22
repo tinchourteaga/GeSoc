0468612aa075a223a64e214b6f77a4f54c6988be
juancueli99 <juanfcue99@gmail.com>
2020-09-22 17:26:37 -0300

    Refactor Sector

--- GeSoc/src/main/java/Dominio/Entidad/Empresa.java
+++ GeSoc/src/main/java/Dominio/Entidad/Empresa.java
@@ -2,6 +2,10 @@ package Dominio.Entidad;
 
 import Dominio.Entidad.Categorias.Categoria;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Empresa extends TipoEntidadJuridica {
 
     private Categoria categoria;
@@ -11,5 +15,15 @@ public class Empresa extends TipoEntidadJuridica {
     }
     public void setCategoria(Categoria categoria) {
         this.categoria = categoria;
+    }
+
+    public void determinarCategoria() {
+
+        List<Categoria> categorias = this.getActividad().getCategorias().
+                stream().filter(unaCategoria ->unaCategoria.getVentasAnuales() >= this.getPromedioVentasAnuales()
+                || unaCategoria.getPersonalMaximo() >= this.getCantidadPersonal()).collect(Collectors.toList());
+
+        this.setCategoria(categorias.stream().max(Comparator.comparingInt(unaCategoria ->
+                unaCategoria.getPersonalMaximo())).get());
     }
 }
--- GeSoc/src/main/java/Dominio/Entidad/Sector.java
+++ GeSoc/src/main/java/Dominio/Entidad/Sector.java
@@ -1,9 +1,38 @@
 package Dominio.Entidad;
 
-public enum Sector{
-    CONSTRUCCION,
-    COMERCIO,
-    SERVICIOS,
-    INDUSTRIA_Y_MINERIA,
-    AGROPECUARIO
+import Dominio.Entidad.Categorias.Categoria;
+
+import java.util.List;
+
+public class Sector {
+
+    private List<Categoria> categorias;
+    private String nombre;
+    private String descripcion;
+
+    public Sector(List<Categoria> categorias, String nombre, String descripcion) {
+        this.categorias = categorias;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public List<Categoria> getCategorias() {
+        return categorias;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void agregarCategoria(Categoria categoria){
+        this.categorias.add(categoria);
+    }
+
+    public void removerCategoria(Categoria categoria){
+        this.categorias.add(categoria);
+    }
 }

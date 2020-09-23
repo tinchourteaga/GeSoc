9adecdc6615ec4bc0d17795ab20fb8c2cead67fa
Matias Lesniak <mlesniak@est.frba.utn.edu.ar>
2020-09-22 23:13:58 -0300

    Cambios en vinculacion e importe que faltaban

--- ProcesoDeVinculacion/importe.py
+++ ProcesoDeVinculacion/importe.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 class Importe:
     codigo=0
     fecha=""
@@ -5,6 +8,25 @@ class Importe:
 
     def __init__(self,codigo,fecha,importe):
         self.codigo=codigo
-        self.fecha=fecha
-        self.importe=importe
+        self.fecha=datetime.strptime(fecha, "%d/%m/%Y")
+        self.importe=float(importe)
+
+    def getValor(self):
+        return self.importe
+
+    def getCodigo(self):
+        return self.codigo
+
+    def getFecha(self):
+        return self.fecha
+
+    def esImporteAplicable(self, importe):
+        if self.importe >= importe:
+            return True
+        else:
+            return False
+
+    def RestarImporte(self,importe):
+        self.importe=self.importe - importe
+
 
--- ProcesoDeVinculacion/vinculacion.py
+++ ProcesoDeVinculacion/vinculacion.py
@@ -4,9 +4,8 @@ import json
 class Vinculacion:
     ingresos=[]
     egresos=[]
-    criterio=""
-    periodoAceptacion=""   #cantidad de dias posterior al ingreso donde vale asociarlo con el egreso.
-
+    criterios=[]
+    periodoMax = 0   #cantidad de dias posterior al ingreso donde vale asociarlo con el egreso.
 
     def __init__(self,jsonInput):
         #data = jsonInput.json()
@@ -17,27 +16,32 @@ class Vinculacion:
         for ingreso in data['ingresos']:
             temp = Importe(ingreso[0],ingreso[1],ingreso[2])
             print("ingreso: ", temp.fecha)
-            self.addIngresos(temp)
+            self.addIngreso(temp)
 
         # Cargo egresos
         for egreso in data['egresos']:
             temp = Importe(egreso[0],egreso[1],egreso[2])
             print("egreso: ", temp.fecha)
-            self.addEgresos(temp)
+            self.addEgreso(temp)
         #Cargo Criterio
         #n criterio aplicar patron creacional, para dar el comportamiento del criterio recibido y guardar instancia.
-        self.criterio=data['criterio']
+        self.criterios=data['criterios']
         #Cargo Periodo de aceptacion
-        self.periodoAceptacion=data['periodo']
-
+        self.periodoMax=data['condicion']
 
 
     def getEgresos(self):
         return self.egresos
     def getIngresos(self):
         return self.ingresos
+    def getCondicion(self):
+        return self.periodoMax
 
-    def addIngresos(self,ingreso):
+    def addIngreso(self,ingreso):
         self.ingresos.append(ingreso)
-    def addEgresos(self,egreso):
+    def addEgreso(self,egreso):
         self.egresos.append(egreso)
+
+    #def asociar(self):
+
+

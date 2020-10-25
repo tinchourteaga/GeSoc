8c02797909d830ae89e42d769d92a20a4d0b96ae
juancueli99 <juanfcue99@gmail.com>
2020-10-24 23:22:49 -0300

    Levanto bien los datos de cargar_proveedor. Faltaría darle una vuelta de rosca al tema de los países, provincias y ciudades

--- GeSoc/src/main/java/Servidor/Controllers/ControllerProveedor.java
+++ GeSoc/src/main/java/Servidor/Controllers/ControllerProveedor.java
@@ -1,17 +1,46 @@
 package Servidor.Controllers;
 
+import API.ML.ControllerMercadoLibre;
+import Lugares.Ciudad;
+import Lugares.Pais;
+import Lugares.Provincia;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ControllerProveedor {
 
     public static ModelAndView visualizarPantalla(Request request, Response response){
 
         Map<String,Object> datos = new HashMap<>();
+        List<String> paises = new ArrayList<>();
+        List<Pais> paisesObjeto= new ArrayList<>();
+
+        List<Provincia> provincias = new ArrayList<>();
+        List<Ciudad> ciudades = new ArrayList<>();
+
+        ControllerMercadoLibre controller = ControllerMercadoLibre.getControllerMercadoLibre();
+
+        paises.addAll(controller.paisesRepo.getTodosLosElementos().stream().map(pais -> (Pais)pais).map(unPais->unPais.getName()).collect(Collectors.toList()));
+        paisesObjeto.addAll(controller.paisesRepo.getTodosLosElementos().stream().map(pais -> (Pais)pais).collect(Collectors.toList()));
+
+        paisesObjeto.forEach(unPais -> {
+            datos.put(unPais.getName(),unPais.getProvincias());
+            provincias.addAll(unPais.getProvincias());
+        });
+
+        provincias.forEach(unaProvincia -> {
+            datos.put(unaProvincia.getName(),unaProvincia.getCiudades());
+            ciudades.addAll(unaProvincia.getCiudades());
+        });
+
+        datos.put("paises",paises);
         ModelAndView vista = new ModelAndView(datos, "cargar_proveedor.html");
 
         return vista;

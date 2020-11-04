058b3623973616b0673e02696af6252754b7806e
jjf-covello <37519105+jjf-covello@users.noreply.github.com>
2020-11-04 18:18:34 -0300

    Probando persistencia en AWS

--- GeSoc/src/test/java/ContextTest/ContextTest.java
+++ GeSoc/src/test/java/ContextTest/ContextTest.java
@@ -7,13 +7,13 @@ import org.uqbarproject.jpa.java8.extras.test.AbstractPersistenceTest;
 import static org.junit.Assert.assertNotNull;
 
 public class ContextTest extends AbstractPersistenceTest implements WithGlobalEntityManager {
-    /*@Test
+    @Test
     public void contextUp() {
         //assertNotNull(entityManager());
     }
 
-    //@Test
+    @Test
     public void contextUpWithTransaction() throws Exception {
         //withTransaction(() -> {});
-    }*/
+    }
 }

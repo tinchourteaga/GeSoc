f028b1b00d84db32daacc2569f4f66fb10fe066c
jjf-covello <covellojorge@gmail.com>
2020-11-09 17:50:52 -0300

    el usuario ve sus msj

--- GeSoc/src/main/java/Servidor/Controllers/ControllerMensajes.java
+++ GeSoc/src/main/java/Servidor/Controllers/ControllerMensajes.java
@@ -16,12 +16,12 @@ import java.util.stream.Collectors;
 public class ControllerMensajes {
 
 
-    public static ModelAndView visualizarPantallaMensajes(Request request, Response response){
+    public static ModelAndView visualizarPantallaMensajes(Request request, Response response) {
 
-        String nombreUsuario= request.session().attribute("nombreUsuario");
+        String nombreUsuario = request.session().attribute("nombreUsuario");
         Usuario usuario = ControllerSesion.obtenerUsuariodeSesion(request);
 
-        if(usuario!=null) {
+        if (usuario != null) {
 
             if (usuario.getRol().getEgresosAREvisar().isEmpty()) {
                 return visualizarPantallaMensajesNoRevisor(request, response);
@@ -29,46 +29,48 @@ public class ControllerMensajes {
                 return visualizarPantallaMensajesRevisor(request, response, usuario);
             }
         }
-        return new ModelAndView(new HashMap<String,Object>(),"pantalla_principal.html");
+        return new ModelAndView(new HashMap<String, Object>(), "pantalla_principal.html");
     }
 
-    public static ModelAndView visualizarPantallaMensajesNoRevisor(Request request, Response response){
+    public static ModelAndView visualizarPantallaMensajesNoRevisor(Request request, Response response) {
 
-        Map<String,Object> datos = new HashMap<>();
+        Map<String, Object> datos = new HashMap<>();
         ModelAndView vista = new ModelAndView(datos, "mensajes_no_revisor.html");
 
         return vista;
     }
 
-    public static ModelAndView visualizarPantallaMensajesRevisor(Request request, Response response,Usuario usuario){
+    public static ModelAndView visualizarPantallaMensajesRevisor(Request request, Response response, Usuario usuario) {
 
         //con el usuario vas sacando datos
-        String fecha=request.queryParams("fechaFiltrado");
-        String egreso=request.queryParams("egreso");
-        List<Mensaje> todosLosMsj=new ArrayList();
-        if(egreso!=null && fecha!=null){
-            todosLosMsj=usuario.getBandejaDeMensajes().getMensajes().stream().filter(msj->msj.getFechaCreado().isBefore(LocalDate.parse(fecha))/*&& msj.getEgreso().getEgreso().equals(Integer.valueOf(egreso))*/).collect(Collectors.toList());
-        }else{
+        String fecha = request.queryParams("fechaFiltrado");
+        String egreso = request.queryParams("egreso");
+        List<Mensaje> todosLosMsj = new ArrayList();
+        if (egreso != null && fecha != null) {
+            todosLosMsj = usuario.getBandejaDeMensajes().getMensajes().stream().filter(msj -> msj.getFechaCreado().isBefore(LocalDate.parse(fecha))/*&& msj.getEgreso().getEgreso().equals(Integer.valueOf(egreso))*/).collect(Collectors.toList());
+        } else {
 
-        if(fecha!=null){
-            todosLosMsj=usuario.getBandejaDeMensajes().filtrarPorFecha(LocalDate.parse(fecha));
-        }else {
-            if(egreso!=null){
-                todosLosMsj=usuario.getBandejaDeMensajes().filtrarPorFecha(LocalDate.parse(fecha));
-        }else{
+            if (fecha != null) {
+                todosLosMsj = usuario.getBandejaDeMensajes().filtrarPorFecha(LocalDate.parse(fecha));
+            } else {
+                if (egreso != null) {
+                    todosLosMsj = usuario.getBandejaDeMensajes().filtrarPorFecha(LocalDate.parse(fecha));
+                } else {
 
-            todosLosMsj = usuario.getBandejaDeMensajes().getMensajes().stream().filter(msj-> msj.getEgreso().getEgreso().equals(Int.valueOf(egreso))).collect(Collectors.toList());
+                    todosLosMsj = usuario.getBandejaDeMensajes().getMensajes().stream().filter(msj -> msj.getEgreso().getEgreso() == (Integer.valueOf(egreso))).collect(Collectors.toList());
+                }
+            }
+            if (request.queryParams("filtrarPorLeidos").equals("true")) {
+
+                todosLosMsj = todosLosMsj.stream().filter(msj -> msj.getLeido()).collect(Collectors.toList());
             }
         }
-        if(request.queryParams("filtrarPorLeidos").equals("true")){
 
-            todosLosMsj=todosLosMsj.stream().filter(msj->msj.getLeido()).collect(Collectors.toList());
-        }
+            Map<String, Object> datos = new HashMap<>();
+            datos.put("mensajes", todosLosMsj);
+            ModelAndView vista = new ModelAndView(datos, "mensajes_revisor.html");
 
-        Map<String,Object> datos = new HashMap<>();
-        datos.put("mensajes",todosLosMsj);
-        ModelAndView vista = new ModelAndView(datos, "mensajes_revisor.html");
+            return vista;
 
-        return vista;
     }
 }

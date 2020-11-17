3210251b3c8052db48c09531b9727481658ec021
tinchourteaga <38136900+tinchourteaga@users.noreply.github.com>
2020-11-17 05:38:04 -0300

    Tengo los huevos inflados

--- GeSoc/src/main/java/Servidor/Servidor.java
+++ GeSoc/src/main/java/Servidor/Servidor.java
@@ -16,7 +16,7 @@ public class Servidor {
     public static void levantarServidor(){
 
         enableDebugScreen();
-        Integer puerto = 15003;
+        Integer puerto = 15005;
         port(puerto);
 
         boolean localhost = true;
